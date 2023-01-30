0ca9fd70135fd067e73ac62e7e5c3ffa0baa3f96
Sbarrachina <sandrabarrachina1978@gmail.com>
2023-01-30 12:22:50 +0100

    creando componente programs

--- Gimnasio_web/src/components/Hero/Hero.jsx
+++ Gimnasio_web/src/components/Hero/Hero.jsx
@@ -1,81 +1,90 @@
-import React from 'react'
-import Header from '../Header/Header'
-import './Hero.css'
-import hero_image from '../../assets/hero_image.png';
-import hero_image_back from '../../assets/hero_image_back.png';
-import Heart from '../../assets/heart.png';
-import Calories from '../../assets/calories.png';
- 
+import React from "react";
+import Header from "../Header/Header";
+import "./Hero.css";
+import hero_image from "../../assets/hero_image.png";
+import hero_image_back from "../../assets/hero_image_back.png";
+import Heart from "../../assets/heart.png";
+import Calories from "../../assets/calories.png";
+
 const Hero = () => {
   return (
-    <div className='hero'>
-        <div className='left-h'>
-        <Header/>
+    <div className="hero">
+      <div className="left-h">
+        <Header />
         {/* the best ad */}
 
         <div className="the-best-ad">
-            <div></div>
-            <span>The best fitness club in the town</span>
+          <div></div>
+          <span>The best fitness club in the town</span>
         </div>
 
         {/* Hero Heading */}
         <div className="hero-text">
-            <div><span className='stroke-text'>
-                Shape</span>
-                <span> Your </span>
-                </div>
-                <div><span>Ideal Body</span>
-                </div>
-                <div>
-                    <span>
-                In here we will help you to shape and build your ideal body and live up your life to fullest
-                </span>
-                </div>
-                 </div>
-       
+          <div>
+            <span className="stroke-text">Shape</span>
+            <span> Your </span>
+          </div>
+          <div>
+            <span>Ideal Body</span>
+          </div>
+          <div>
+            <span>
+              In here we will help you to shape and build your ideal body and
+              live up your life to fullest
+            </span>
+          </div>
+        </div>
+
         {/*figures  */}
         <div className="figures">
-            <div>
+          <div>
             <span>+140</span>
             <span>expert coachs</span>
-            </div>
+          </div>
 
-            <div>
+          <div>
             <span>+978</span>
             <span>members joined</span>
-            </div>
+          </div>
 
-            <div>
+          <div>
             <span>+50</span>
             <span>fitness programs</span>
-            </div>
+          </div>
         </div>
 
         {/*hero buttons   */}
         <div className="hero-buttons">
-            <buttons className="btn">Get Started</buttons>
-            <buttons className="btn">Learn More</buttons>
+          <buttons className="btn">Get Started</buttons>
+          <buttons className="btn">Learn More</buttons>
+        </div>
+      </div>
+
+      <div className="right-h">
+        <button className="btn">Join Now</button>
+
+        <div className="heart-rate">
+          <img src={Heart} alt="" />
+          <span>Heart Rate</span>
+          <span> 116 bpm</span>
         </div>
 
+        {/* hero images */}
+        <img src={hero_image} className="hero-image" alt="" />
+        <img src={hero_image_back} className="hero-image-back" alt="" />
 
+        {/* calories */}
+
+        <div className="calories">
+          <img src={Calories} alt="" />
+          <div>
+            <span>Calories Burned</span>
+            <span>220 kcal</span>
+          </div>
         </div>
-
-        <div className='right-h'>
-        <button className='btn'>Join Now</button>
-
-   <div className="heart-rate">
-    <img src={Heart} alt="" />
-    <span>Heart Rate</span>
-    <span> 116 bpm</span>
-   </div>
-
-   {/* hero images */}
-   
-
+      </div>
     </div>
-    </div>
-
   );
-}
+};
 
-export default Hero
+export default Hero;
