830861b4d22b04049600feb50bbb356bf8cae90d
Sbarrachina <sandrabarrachina1978@gmail.com>
2023-01-31 13:55:36 +0100

    componente testimonials, plans, formulario email creado y estilos

--- Gimnasio_web/src/App.js
+++ Gimnasio_web/src/App.js
@@ -2,12 +2,19 @@ import './App.css';
 import Hero from './components/Hero/Hero';
 import Programs from './components/Programs/Programs';
 import Reasons from './components/Reasons/Reasons';
+import Plans from './components/Plans/Plans';
+import Testimonials from './assets/Testimonials/Testimonials';
+import Join from './components/Join/Join';
+
 function App() {
   return (
     <div className="App">
       <Hero/>
       <Programs/>
       <Reasons/>
+      <Plans/>
+      <Testimonials/>
+      <Join/>
 
     </div>
   );
