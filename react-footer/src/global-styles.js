27e9af238498930841f1386091917eaa484b643f
Sbarrachina <sandrabarrachina1978@gmail.com>
2023-02-21 18:43:04 +0100

    add react footer page

---
+++ react-footer/src/global-styles.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  html, body {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    font-family: 'Nunito Sans', sans-serif;
+    position: relative;
+    top: 300px;
+    background-size: cover;
+    background-image: url("https://png.pngtree.com/thumb_back/fh260/background/20210814/pngtree-blue-purple-simple-gradient-background-image_760572.jpg");}`;
+    
+  
