b723f6cddedf486e2c44505cb87b4c12d399c6d1
Sbarrachina <sandrabarrachina1978@gmail.com>
2023-03-10 11:06:37 +0100

    cambios de nombre en los proyectos del 11 al 19

--- 14-slider-with-bootstrap/src/components/NavComponent.js
+++ 14-slider-with-bootstrap/src/components/NavComponent.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Navbar, Container, Offcanvas, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap'
+import { Navbar, Container } from 'react-bootstrap'
 import '../components/NavComponent.css';
 const NavComponent = () => {
     return (
